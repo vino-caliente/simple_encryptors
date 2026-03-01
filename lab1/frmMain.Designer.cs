527001f1e6b239f8fb3c1fecb9585ce499b1474d
vino-caliente <7145683@gmail.com>
2026-03-02 01:47:24 +0300

    Initial commit

---
+++ lab1/frmMain.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace lab1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxAlg = new System.Windows.Forms.ComboBox();
+            this.labelAlg = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxAlg
+            // 
+            this.comboBoxAlg.BackColor = System.Drawing.SystemColors.Info;
+            this.comboBoxAlg.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAlg.FormattingEnabled = true;
+            this.comboBoxAlg.Items.AddRange(new object[] {
+            "Метод децимаций",
+            "Шифр Виженера"});
+            this.comboBoxAlg.Location = new System.Drawing.Point(255, 27);
+            this.comboBoxAlg.Name = "comboBoxAlg";
+            this.comboBoxAlg.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxAlg.TabIndex = 0;
+            // 
+            // labelAlg
+            // 
+            this.labelAlg.AutoSize = true;
+            this.labelAlg.Location = new System.Drawing.Point(61, 27);
+            this.labelAlg.Name = "labelAlg";
+            this.labelAlg.Size = new System.Drawing.Size(178, 13);
+            this.labelAlg.TabIndex = 1;
+            this.labelAlg.Text = "Выберите алгоритм шифрования:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(64, 141);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(312, 115);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(64, 116);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(92, 13);
+            this.labelInput.TabIndex = 3;
+            this.labelInput.Text = "Исходный текст:";
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(64, 68);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(36, 13);
+            this.labelKey.TabIndex = 4;
+            this.labelKey.Text = "Ключ:";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(129, 68);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(247, 20);
+            this.textBoxKey.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Результирующий текст:";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(67, 308);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(309, 112);
+            this.textBoxOutput.TabIndex = 7;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(255, 106);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(121, 23);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "Читать из файла";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(255, 277);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(120, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Сохранить в файл";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(67, 448);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(125, 23);
+            this.btnEncrypt.TabIndex = 10;
+            this.btnEncrypt.Text = "Шифровать";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(67, 477);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(125, 23);
+            this.btnDecrypt.TabIndex = 11;
+            this.btnDecrypt.Text = "Дешифровать";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(299, 448);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(448, 545);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelAlg);
+            this.Controls.Add(this.comboBoxAlg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxAlg;
+        private System.Windows.Forms.Label labelAlg;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ lab1/frmMain.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class frmMain : Form
+    {
+        private const int n = 33;
+        private string ReadFile;
+        private string WriteFile;
+        private const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            comboBoxAlg.SelectedIndex = 0;
+            ReadFile = "";
+            WriteFile = "";
+        }
+
+        private string Format_Str(string str)
+        {
+            str = str.ToUpper();
+            string res = Regex.Replace(str, @"[^А-ЯЁ]", "");
+            return res;
+        }
+
+        private int Format_Int_Key(string str)
+        {
+            string res = Regex.Replace(str, @"\D", "");
+            if (res != "")
+                return int.Parse(res);
+            else
+                return 0;
+        }
+
+        private int gcd(int a, int b)
+        {
+            while (a!=0 && b!=0)
+                if (a > b)
+                    a = a % b;
+                else
+                    b = b % a;
+            return a + b;
+        }
+
+        private string Format_Vigenere_Key(string key, string input_text)
+        {
+            if (key.Length >= input_text.Length)
+                return key.Substring(0, input_text.Length);
+            else
+                return key + input_text.Substring(0, input_text.Length - key.Length);
+        }
+
+        private string Decimation_Encrypt(string str, int key)
+        {
+            char[] res = new char[str.Length];
+
+            for (int i=0; i<str.Length; i++)
+            {
+                int code = alphabet.IndexOf(str[i]) * key % n;
+                res[i] = alphabet[code];
+            }
+
+            return new string(res);
+        }
+
+        int gcd_extended(int a, int b, out int x, out int y)
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+            int x1, y1;
+            int d = gcd_extended(b % a, a, out x1, out y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return d;
+        }
+
+        private string Decimation_Decrypt(string str, int key)
+        {
+            // (key * reverse_key) mod n = 1
+            // Расширенный алгоритм евклида: ax + by = gcd(a,b)
+            // Для децимации:                (key*reverse_rey + n*y) mod n = 1
+            int x, y;
+            gcd_extended(key, n, out x, out y);
+            // x(reverse_key) может получиться отрицательным, поэтому:
+            x = (x + n) % n;
+            // далее делаем то же самое, что и при шифровании, только с обратным ключом(x)
+            return Decimation_Encrypt(str, x);
+        }
+
+        private string Vigenere_Encryption(string str, string key)
+        {
+            char[] res = new char[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int code = (alphabet.IndexOf(str[i]) + alphabet.IndexOf(key[i])) % n;
+                res[i] = alphabet[code];
+            }
+
+            return new string(res);
+        }
+
+        private string Vigenere_Decryption(string str, string key)
+        {
+            char[] res = new char[str.Length];
+            char[] fullkey = key.ToCharArray();
+            Array.Resize(ref fullkey, str.Length);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                int code = (alphabet.IndexOf(str[i]) - alphabet.IndexOf(fullkey[i]) + n) % n;
+                res[i] = alphabet[code];
+                if (i+key.Length < fullkey.Length)
+                    fullkey[i + key.Length] = alphabet[code];  // добавление расшифрованного символа в ключ
+            }
+
+            return new string(res);
+        }
+
+        private string Get_Input()
+        {
+            string content = "";
+            if (ReadFile == "")
+                content = Format_Str(textBoxInput.Text);
+            else
+            {
+                try
+                {
+                    content = Format_Str(System.IO.File.ReadAllText(ReadFile));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка {ex.Message} при чтении из файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ReadFile = "";
+                textBoxInput.ReadOnly = false;
+                textBoxInput.Text = content;
+            }
+            return content;
+        }
+
+        private void Write_Output(string str)
+        {
+            textBoxOutput.Text = str;
+            if (WriteFile != "")
+            {
+                try
+                {
+                    System.IO.File.WriteAllText(WriteFile, str);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка {ex.Message} при сохранении в файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                textBoxOutput.ReadOnly = false;
+                WriteFile = "";
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt";
+                openFileDialog.FilterIndex = 1;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ReadFile = openFileDialog.FileName;
+                    textBoxInput.Text = "Чтение из файла: " + ReadFile;
+                    textBoxInput.ReadOnly = true;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt";
+                saveFileDialog.FilterIndex = 1;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    WriteFile = saveFileDialog.FileName;
+                    textBoxOutput.Text = "Запись в файл: " + WriteFile;
+                    textBoxOutput.ReadOnly = true;
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = "";
+            textBoxOutput.Text = "";
+            ReadFile = "";
+            WriteFile = "";
+            textBoxInput.ReadOnly = false;
+            textBoxOutput.ReadOnly = false;
+            textBoxKey.Text = "";
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (comboBoxAlg.SelectedIndex == 0) // Decimation
+            {
+                int key = Format_Int_Key(textBoxKey.Text);
+                if (gcd(key, n) != 1)
+                    MessageBox.Show($"Ошибка! Ключ должен быть взаимно простым с количеством букв в алфавите({n})", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    string input_text = Get_Input();
+                    string cipher_text = Decimation_Encrypt(input_text, key);
+                    Write_Output(cipher_text);
+                }
+            }
+            else //Vigenere
+            {
+                string input_text = Get_Input();
+                string key = Format_Str(textBoxKey.Text);
+                if (key == "")
+                    MessageBox.Show($"Ошибка! Ключ должен содержать хотя бы 1 русскую букву", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    key = Format_Vigenere_Key(key, input_text);
+                    string cipher_text = Vigenere_Encryption(input_text, key);
+                    Write_Output(cipher_text);
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (comboBoxAlg.SelectedIndex == 0) // Decimation
+            {
+                int key = Format_Int_Key(textBoxKey.Text);
+                if (gcd(key, n) != 1)
+                    MessageBox.Show($"Ошибка! Ключ должен быть взаимно простым с количеством букв в алфавите({n})", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    string input_text = Get_Input();
+                    string decrypted_text = Decimation_Decrypt(input_text, key);
+                    Write_Output(decrypted_text);
+                }
+            }
+            else //Vigenere
+            {
+                string input_text = Get_Input();
+                string key = Format_Str(textBoxKey.Text);
+                if (key == "")
+                    MessageBox.Show($"Ошибка! Ключ должен содержать хотя бы 1 русскую букву", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    string decrypted_text = Vigenere_Decryption(input_text, key);
+                    Write_Output(decrypted_text);
+                }
+            }
+        }
+    }
+}
